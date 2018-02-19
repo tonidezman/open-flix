78f2b56c5a1b68e57db8ab8f09cbbd38c57d1a37
Toni Dezman <toni.dezman@gmail.com>
2018-02-19 18:32:24 +0100

    adds videos show page

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,4 +2,8 @@ class VideosController < ApplicationController
   def index
     @videos = Video.all.limit(6)
   end
+
+  def show
+    @video = Video.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get 'home', to: 'videos#index'
 
+  resources :videos, only: :show
+
   get 'ui(/:action)', controller: 'ui'
 end
