367e0f350c4204eccad6f0427d38fbfb858705de
Toni Dezman <toni.dezman@gmail.com>
2018-02-20 07:25:57 +0100

    adds comedies controller and show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
 
+  get 'categories/show'
+
   get 'home', to: 'videos#index'
 
   resources :videos, only: :show
+  resources :categories, only: :show
 
   get 'ui(/:action)', controller: 'ui'
 end
