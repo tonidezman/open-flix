e96413f0a2f6ade768bbafc5cedf2358647215da
Toni Dezman <toni.dezman@gmail.com>
2018-02-20 09:04:23 +0100

    simple test for video model

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Video, type: :model do
+  it 'save itself' do
+    category = Category.create(name: 'Comedy')
+    video = Video.create(title: 'Movie3', category: category)
+    expect(Video.first).to eql(video)
+    expect(Video.count).to be(1)
+    expect(Category.count).to be(1)
+  end
+
+  it "video's and categories are deleted" do
+    expect(Video.count).to be(0)
+    expect(Category.count).to be(0)
+  end
 end
