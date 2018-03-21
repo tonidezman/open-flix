0a1121ba5018392bb5afcd4cdd156809fdd35ea3
Toni Dezman <toni.dezman@gmail.com>
2018-03-21 10:14:00 +0100

    changes mailgun domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,15 +63,15 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'openflix.herokuapp.com' }
   config.action_mailer.perform_caching = false
 
+  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'openflix.heroku.com',
+    :domain         => ENV['MAILGUN_DOMAIN'],
     :authentication => :plain,
   }
-  ActionMailer::Base.delivery_method = :smtp
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
